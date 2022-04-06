b0da0ecdf72013a73ac99f9cfd9121a4df009a74
RhinoLee <picklerhino@gmail.com>
2022-04-06 12:59:52 +0800

    整理 line login & notify URL

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,6 @@ import { storeToRefs } from 'pinia'
 import axios from "axios"
 
 const userStore = useUserStore()
-const lineLoginParams = {
-  url: "https://access.line.me/oauth2/v2.1/authorize",
-  response_type: "code",
-  client_id: import.meta.env.VITE_LINE_CLIENT_ID,
-  redirect_uri: "http://localhost:3000/login/line/return",
-  state: "12345",
-  scope: "profile%20openid%20email",
-}
-const lineNotifyParams = {
-  url: "https://notify-bot.line.me/oauth/authorize",
-  response_type: "code",
-  client_id: import.meta.env.VITE_LINE_NOTIFY_ID,
-  redirect_uri: "http://localhost:3000/notify/line/return",
-  state: "12345",
-  scope: "notify",
-}
-
-const lineLoginUrl = `${lineLoginParams.url}?response_type=${lineLoginParams.response_type}&client_id=${lineLoginParams.client_id}&redirect_uri=${lineLoginParams.redirect_uri}&state=${lineLoginParams.state}&scope=${lineLoginParams.scope}}`
-const lineNotifyUrl = `${lineNotifyParams.url}?response_type=${lineNotifyParams.response_type}&client_id=${lineNotifyParams.client_id}&redirect_uri=${lineNotifyParams.redirect_uri}&state=${lineNotifyParams.state}&scope=${lineNotifyParams.scope}`
-userStore.lineLoginUrl = lineLoginUrl
-userStore.lineNotifyUrl = lineNotifyUrl
 
 onMounted(async () => {
   // 檢查 localStorage 是否有 token，有的話跟 server 要 userInfo，沒有則顯示登入 UI
--- src/stores/user.js
+++ src/stores/user.js
@@ -8,10 +8,36 @@ export const useUserStore = defineStore({
     isLogin: false,
     isNotify: false,
     userInfo: {},
+    lineLoginParams: {
+      response_type: "code",
+      client_id: import.meta.env.VITE_LINE_CLIENT_ID,
+      redirect_uri: "http://localhost:3000/login/line/return",
+      state: "12345",
+      scope: "profile openid email",
+    },
+    lineNotifyParams: {
+      response_type: "code",
+      client_id: import.meta.env.VITE_LINE_NOTIFY_ID,
+      redirect_uri: "http://localhost:3000/notify/line/return",
+      state: "12345",
+      scope: "notify",
+    },
     lineLoginUrl: "",
     lineNotifyUrl: "",
   }),
   getters: {
+    lineLoginUrl(state) {
+      const loginUrl = new URL(import.meta.env.VITE_LINE_LOGIN_ENDPOINT)
+      const searchParams = new URLSearchParams(state.lineLoginParams)
+      loginUrl.search = searchParams
+      return loginUrl.href
+    },
+    lineNotifyUrl(state) {
+      const notifyUrl = new URL(import.meta.env.VITE_LINE_NOTIFY_ENDPOINT)
+      const searchParams = new URLSearchParams(state.lineNotifyParams)
+      notifyUrl.search = searchParams
+      return notifyUrl.href
+    }
   },
   actions: {
     async cancelNotify() {

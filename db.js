dc90fc4b2bbc4c740dfaa611542da67e7e98bdc3
RhinoLee <picklerhino@gmail.com>
2022-04-07 20:36:10 +0800

    push to heroku

--- db.js
+++ db.js
@@ -2,12 +2,16 @@ const { Pool } = require("pg");
 const dotenv = require('dotenv');
 dotenv.config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USERNAME,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-});
+  connectionString: process.env.DATABASE_URL,
+})
 
 module.exports = pool;

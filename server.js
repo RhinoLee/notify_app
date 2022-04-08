afd4683478c3656920cbbd3669b632da2982503d
RhinoLee <picklerhino@gmail.com>
2022-04-08 16:23:58 +0800

    add history-connect to server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const history = require('connect-history-api-fallback');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 const port = process.env.PORT || 3000;
 app.listen(port);

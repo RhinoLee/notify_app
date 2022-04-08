586b3a09193209f5f334b7f07874d9381a4166dd
RhinoLee <picklerhino@gmail.com>
2022-04-09 02:10:39 +0800

    test

--- controller/joke.js
+++ controller/joke.js
@@ -1,9 +1,6 @@
-const https = require('https');
 const axios = require("axios")
 const cheerio = require('cheerio');
 const { randomBytes } = require("crypto");
-
-https.globalAgent.options.rejectUnauthorized = false;
 
 const jokeController = {
   getJokes: async (res, req) => {
@@ -13,41 +10,41 @@ const jokeController = {
       bsn: 60555,
       snA: 3105
     }
+    try {
+      const result = await axios.get(api, { params })
+      const jokes = []
+      const $ = cheerio.load(`${result.data}`);
+      const q = $(".c-article__content > div").eq(3).children("div").length
+      const regexpQ = /^[0-9].+/gm
+      const regexpA = /^答.+/gm
+      for (let i = 0; i < q; i++) {
+        const textA = $(".c-article__content > div").eq(3).children("div").eq(4 * i + 1).children("font").text()
+        const textQ = $(".c-article__content > div").eq(3).children("div").eq(4 * i - 1).children("font").text()
+        if (Boolean(textA) && i > 0 || Boolean(textQ) && i > 0) {
+          let obj = {}
+          if (regexpQ.exec(textQ)) {
+            obj.q = textQ.split(".")[1]
+          }
+          if (regexpA.exec(textA)) {
+            obj.a = textA
+          }
 
-    const result = await axios.get(api, { params } )
-
-    const jokes = []
-    const $ = cheerio.load(`${result.data}`);
-    const q = $(".c-article__content > div").eq(3).children("div").length
-    const regexpQ = /^[0-9].+/gm
-    const regexpA = /^答.+/gm
-    for (let i = 0; i < q; i++) {
-      const textA = $(".c-article__content > div").eq(3).children("div").eq(4 * i + 1).children("font").text()
-      const textQ = $(".c-article__content > div").eq(3).children("div").eq(4 * i - 1).children("font").text()
-      if (Boolean(textA) && i > 0 || Boolean(textQ) && i > 0) {
-        let obj = {}
-        if (regexpQ.exec(textQ)) {
-          obj.q = textQ.split(".")[1]
-        }
-        if (regexpA.exec(textA)) {
-          obj.a = textA
-        }
-
-        if (obj.q && obj.a) {
-          jokes.push(obj)
+          if (obj.q && obj.a) {
+            jokes.push(obj)
+          }
         }
       }
+
+      const jokesLength = jokes.length
+      if (jokesLength > 0) {
+        const randomNum = Math.ceil(Math.random() * jokesLength - 1)
+        console.log("randomNum", randomNum);
+        return jokes[randomNum]
+      }
+    } catch (err) {
+      console.log(err);
+      return false
     }
-
-    const jokesLength = jokes.length
-    if (jokesLength > 0) {
-      const randomNum = Math.ceil(Math.random() * jokesLength - 1)
-      console.log("randomNum", randomNum);
-      return jokes[randomNum]
-    }
-
-
-    return false
   }
 }
 

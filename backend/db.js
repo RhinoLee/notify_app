487b2dbf50c3c0cddfa64edf1f168fd29ac9a7d3
RhinoLee <picklerhino@gmail.com>
2022-04-11 16:49:48 +0800

    enviroment setting

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 let pool;
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USERNAME,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-// });
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
